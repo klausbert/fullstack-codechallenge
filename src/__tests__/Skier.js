4c6b9c2cc22307672eef52ef0c03b77373ba2379
Klaus Pieslinger <klaus@pieslinger.net>
2019-12-11 19:14:35 -0300

    Added test for Crash-Left error

---
+++ src/__tests__/Skier.js
@@ -0,0 +1,34 @@
+import * as Constants from "../Constants";
+import { Skier } from '../Entities/Skier'
+
+const skier = new Skier(0, 0);
+
+test('Skier equals something', () => {
+    console.log('skier log', skier)
+    // expect(skier.direction).toBe(3);
+    // expect(skier).toBe({ x: 0, y: 0, assetName: 'skierDown', direction: 3, speed: 10 });
+    expect(skier.x).toBe(0);
+    expect(skier.y).toBe(0);
+    expect(skier.assetName).toBe('skierDown');
+    expect(skier.direction).toBe(3);
+    expect(skier.speed).toBe(10);
+    
+
+    skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH)   //  CRASH
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.CRASH);
+    
+    skier.turnRight()
+    expect(skier.assetName).toBeDefined();
+    
+    skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH)   //  CRASH
+    skier.turnUp()
+    expect(skier.assetName).toBeDefined();
+    
+    skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH)   //  CRASH
+    skier.turnDown()
+    expect(skier.assetName).toBeDefined();
+    
+    skier.setDirection(Constants.SKIER_DIRECTIONS.CRASH)   //  CRASH
+    skier.turnLeft()
+    expect(skier.assetName).toBeDefined();
+});
